5ea593e3e3fee3beecd7b67e16faa149df2f55e7
charming-input-2976 <mbingramm@gmail.com>
2020-12-01 19:26:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,18 +1,17 @@
 require 'yaml'
 
-def load_library(emoticon_file)
-  emoticons = YAML.load_file('./lib/emoticons.yml')
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_hash = Hash.new
 
-  emoticon_lib = {'get_meaning'  => {},
-                  'get_emoticon' => {} }
+  emoticon_hash["get_emoticon"] = Hash.new
+  emoticon_hash["get_meaning"] = Hash.new
 
-  emoticons.each do |meaning, value|
-    english = value[0]
-    japanese = value[1]
-    emoticon_lib['get_meaning'][japanese] = meaning
-    emoticon_lib['get_emoticon'][english] = japanese
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
   end
-  emoticon_lib
+  emoticon_hash
 end
   
 end
